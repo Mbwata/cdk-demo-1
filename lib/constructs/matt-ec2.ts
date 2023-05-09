c8f7d0344694ff5ea52187bf22142b9d754ba8ee
Matt H <44037139+Mbwata@users.noreply.github.com>
2023-05-09 09:55:39 -0500

    restrict to just ubuntu or rhel os

--- lib/constructs/matt-ec2.ts
+++ lib/constructs/matt-ec2.ts
@@ -6,7 +6,7 @@ export interface ec2Props {
     readonly instanceSize?: InstanceSize;
     readonly instanceClass?: InstanceClass;
     readonly vpc: ec2.IVpc
-    readonly ami: ec2.IMachineImage
+    readonly os?: 'rhel' | 'ubuntu'
     readonly rsaKey?: ec2.CfnKeyPair
 };
 
@@ -14,6 +14,25 @@ export class MattEc2 extends Construct {
     public rsaKey: ec2.CfnKeyPair;
     constructor(scope: Construct, id: string, props: ec2Props) {
         super(scope, id);
+
+
+        const ami = props.os === 'rhel' ?
+            ec2.MachineImage.lookup({
+                name: 'RHEL-8*_HVM-*-arm64-*-Hourly2-GP2',
+                owners: ['309956199498'], // Red Hat
+            }) : props.os === 'ubuntu' ?
+                ec2.MachineImage.lookup({
+                    name: 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*',
+                    owners: ['099720109477'], // Canonical
+                }) : ec2.MachineImage.lookup({
+                    name: 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*',
+                    owners: ['099720109477'], // Canonical
+                })
+
+
+
+
+
         const instanceClass = props?.instanceClass ?? InstanceClass.BURSTABLE4_GRAVITON
         const instanceSize = props?.instanceSize ?? InstanceSize.NANO
         const sg = new ec2.SecurityGroup(this, 'EC2SecurityGroup', {
@@ -28,7 +47,7 @@ export class MattEc2 extends Construct {
         const ec2instance = new ec2.Instance(this, 'EC2Instance', {
             instanceType: InstanceType.of(instanceClass, instanceSize),
             vpc: props.vpc,
-            machineImage: props.ami,
+            machineImage: ami,
             securityGroup: sg,
             keyName: 'new-key'
         });
--- lib/stacks/minecraft-stack.ts
+++ lib/stacks/minecraft-stack.ts
@@ -7,17 +7,11 @@ export class MineCraftStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const ubuntuAmi = ec2.MachineImage.lookup({
-            name: 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*',
-            //name: 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*',
-            owners: ['099720109477'], // Canonical
-        });
-
         new MattEc2(this, 'MinecraftEC2', {
             vpc: ec2.Vpc.fromLookup(this, 'vpc', {
                 vpcId: 'vpc-19186463'
             }),
-            ami: ubuntuAmi
+            //os: 'ubuntu'
         })
     }
 }
