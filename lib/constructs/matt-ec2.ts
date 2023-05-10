ed760eaa47195523b6ca21364f3e21293aab98d0
Matt H <44037139+Mbwata@users.noreply.github.com>
2023-05-10 13:27:37 -0500

    this creates a working minecraft ec2

--- lib/constructs/matt-ec2.ts
+++ lib/constructs/matt-ec2.ts
@@ -1,55 +1,47 @@
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import { InstanceClass, InstanceSize, InstanceType, SecurityGroup } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export interface ec2Props {
     readonly instanceSize?: InstanceSize;
-    readonly instanceClass?: InstanceClass;
+    //readonly instanceClass?: InstanceClass;
     readonly vpc: ec2.IVpc
+    readonly arch?: 'arm64' | 'amd64'
     readonly os?: 'rhel' | 'ubuntu'
     readonly rsaKey?: ec2.CfnKeyPair
 };
 
 export class MattEc2 extends Construct {
     public rsaKey: ec2.CfnKeyPair;
+    public securityGroup: SecurityGroup;
+    addUserData: any;
+    public ec2instance: ec2.Instance;
+
     constructor(scope: Construct, id: string, props: ec2Props) {
         super(scope, id);
 
-
         const ami = props.os === 'rhel' ?
             ec2.MachineImage.lookup({
-                name: 'RHEL-8*_HVM-*-arm64-*-Hourly2-GP2',
+                name: `RHEL-8*_HVM-*-${props.arch === 'arm64' ? 'arm64' : 'x86_64'}-*-Hourly2-GP2`,
                 owners: ['309956199498'], // Red Hat
-            }) : props.os === 'ubuntu' ?
-                ec2.MachineImage.lookup({
-                    name: 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*',
-                    owners: ['099720109477'], // Canonical
-                }) : ec2.MachineImage.lookup({
-                    name: 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*',
-                    owners: ['099720109477'], // Canonical
-                })
+            }) : ec2.MachineImage.lookup({
+                name: `ubuntu/images/hvm-ssd/ubuntu-focal-20.04-${props.arch === 'arm64' ? 'arm64' : 'amd64'}-server-*`,
+                owners: ['099720109477'], // Canonical
+            })
 
-
-
-
-
-        const instanceClass = props?.instanceClass ?? InstanceClass.BURSTABLE4_GRAVITON
+        const instanceClass = props.arch === 'arm64' ? InstanceClass.T4G : InstanceClass.T2
         const instanceSize = props?.instanceSize ?? InstanceSize.NANO
-        const sg = new ec2.SecurityGroup(this, 'EC2SecurityGroup', {
+        this.securityGroup = new ec2.SecurityGroup(this, 'EC2SecurityGroup', {
             vpc: props.vpc,
             allowAllOutbound: true,
         });
-        sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access');
-        // this.rsaKey = props.rsaKey ?? new ec2.CfnKeyPair(this, 'rsaKey', {
-        //     keyName: 'stanly',
-        //     keyType: 'rsa',
-        // })
-        const ec2instance = new ec2.Instance(this, 'EC2Instance', {
+        this.ec2instance = new ec2.Instance(this, 'EC2Instance', {
             instanceType: InstanceType.of(instanceClass, instanceSize),
             vpc: props.vpc,
             machineImage: ami,
-            securityGroup: sg,
-            keyName: 'new-key'
+            securityGroup: this.securityGroup,
+            keyName: 'new-key',
+        
         });
     }
 };
--- lib/stacks/minecraft-stack.ts
+++ lib/stacks/minecraft-stack.ts
@@ -7,11 +7,19 @@ export class MineCraftStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new MattEc2(this, 'MinecraftEC2', {
+        const minecraftServer = new MattEc2(this, 'MinecraftEC2', {
             vpc: ec2.Vpc.fromLookup(this, 'vpc', {
                 vpcId: 'vpc-19186463'
             }),
-            //os: 'ubuntu'
+            os: 'ubuntu',
+            //arch: 'arm64'
         })
+        //add ingress rules
+        minecraftServer.securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access');
+        minecraftServer.securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(19132), 'open minecraft TCP port');
+        minecraftServer.securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.udp(19132), 'open minecraft UDP port');
+        minecraftServer.ec2instance.addUserData(`su - ubuntu -c 'curl https://raw.githubusercontent.com/Mbwata/cdk-demo-1/main/scripts/SetupMinecraft.sh | bash'`);
     }
+
+
 }
